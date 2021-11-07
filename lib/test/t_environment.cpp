072515a32bdef294abea92085c6becf55ffb148b
adrian.schneider@aot.swiss <adrian.schneider@aot.swiss>
2021-11-07 12:01:18 +0100

    Introduce environment

---
+++ lib/test/t_environment.cpp
@@ -0,0 +1,17 @@
+
+#include <gtest/gtest.h>
+#include "environment.h"
+
+TEST(Environment, Id)
+{
+    auto e = Environment::createEnvironment(9);
+    ASSERT_EQ(e->id(), 9);
+}
+
+TEST(Environment, MovePossibleBaseImpl)
+{
+    auto e = Environment::createEnvironment(9);
+    auto[possible, newPos] = e->possibleMove(Eigen::Vector2d(5.0, 9.0));
+    ASSERT_TRUE(possible);
+    ASSERT_TRUE((newPos - Eigen::Vector2d(5.0, 9.0)).isMuchSmallerThan(0.0001));
+}
